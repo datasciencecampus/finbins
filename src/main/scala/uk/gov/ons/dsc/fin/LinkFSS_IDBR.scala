13ca516731065ed3727e47489117dd6c0f033c80
Alex N <noyva@C02STF9LGTFM.local>
2017-06-13 15:25:55 +0100

    linking fss-idbr

--- src/main/scala/uk/gov/ons/dsc/fin/LinkFSS_IDBR.scala
+++ src/main/scala/uk/gov/ons/dsc/fin/LinkFSS_IDBR.scala
@@ -23,7 +23,7 @@ object LinkFSS_IDBR {
 
     val fss = sqlContext.read.load("fss0")
 
-    val joinExpr = fss.col("RUReference") === idbr.col("C0")
+    val joinExpr = fss.col("RUReference") === idbr.col("Id")
 
     val fss_idbr = fss.join(idbr,joinExpr)
 
