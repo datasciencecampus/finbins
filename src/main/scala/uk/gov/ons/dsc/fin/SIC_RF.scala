fd7e14565e2f9571df88dccb3c6d46f25a71752c
Alex N <noyva@C02STF9LGTFM.local>
2017-07-11 17:56:20 +0100

    combinations

--- src/main/scala/uk/gov/ons/dsc/fin/SIC_RF.scala
+++ src/main/scala/uk/gov/ons/dsc/fin/SIC_RF.scala
@@ -6,9 +6,8 @@ import org.apache.spark.ml.{Pipeline, PipelineStage}
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode}
-import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.{avg, col, udf}
 import org.apache.spark.mllib.linalg.Vectors
-
 import org.apache.spark.sql.Column
 //import uk.gov.ons.dsc.fin.SICNaiveBayes.{cvModelName, indexer_label, modelNB, traingEval}
 
@@ -61,7 +60,11 @@ object SIC_RF {
       // .withColumn("features")
       //   .withColumn("features",toVec4(fssIDBR(""),fssIDBR("")))
       .dropDuplicates(Array("CompanyName"))
-    .na.fill(0.0)
+    .na.fill(0)
+
+    val featureCols = fssIDBR.dtypes.filter(f=>  f._2=="DoubleType").map(f=>f._1)
+
+    val featuresCombIter = featureCols.combinations(3)
 
     fssIDBR.registerTempTable("fss_idbr")
 
@@ -70,11 +73,20 @@ object SIC_RF {
     trainingData.cache.count
     testData.cache.count
 
+    var fCols:Array[String] = null
 
+    while (featuresCombIter.hasNext) {
 
-    val fssPred =traingEval(Array( assembler,indexer_label, modelRF.setFeaturesCol("features")), trainingData, testData, sqlContext)
-    fssPred.write.mode(SaveMode.Overwrite).save("SIC_predictions")
+      fCols = featuresCombIter.next()
 
+      assembler.setInputCols(fCols)
+
+      val fssPred = traingEval(Array(assembler, indexer_label, modelRF.setFeaturesCol("features")), trainingData, testData, sqlContext)
+      println(" Overall accuracy"+fCols.toString)
+      fssPred.select(avg( (col("numCorrect") / col("total")))).show
+
+      //   fssPred.write.mode(SaveMode.Overwrite).save("SIC_predictions")
+    }
 
   }
 
@@ -87,9 +99,9 @@ object SIC_RF {
 
       val predictions = model.transform(testData)
 
-      predictions.write.mode(SaveMode.Overwrite).save("predictions_RF_raw")
+      // predictions.write.mode(SaveMode.Overwrite).save("predictions_RF_raw")
 
-      println("predictions_RF_raw saved ...")
+      println("predictions_made ...")
 
       predictions.registerTempTable("predictions")
 
